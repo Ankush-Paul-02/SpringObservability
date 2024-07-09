27d6f0220b4dc0d8580c9771a3d1e5395dd2656b
Ankush-Paul-02 <ankushpaulclg2002@gmail.com>
2024-07-09 16:08:51 +0530

    registry new timer to create custom metrics

--- src/main/java/com/devmare/springobservality/SpringObservalityApplication.java
+++ src/main/java/com/devmare/springobservality/SpringObservalityApplication.java
@@ -2,12 +2,20 @@ package com.devmare.springobservality;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 public class SpringObservalityApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(SpringObservalityApplication.class, args);
+    }
+
+    @Bean
+    public RestTemplate restTemplate(RestTemplateBuilder restTemplateBuilder) {
+        return restTemplateBuilder.build();
     }
 
 }
--- src/main/java/com/devmare/springobservality/controller/DevController.java
+++ src/main/java/com/devmare/springobservality/controller/DevController.java
@@ -1,18 +1,34 @@
 package com.devmare.springobservality.controller;
 
+import io.micrometer.core.annotation.Timed;
 import io.micrometer.observation.Observation;
 import io.micrometer.observation.ObservationRegistry;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.concurrent.TimeUnit;
+
 @RestController
+@Slf4j
 @RequiredArgsConstructor
 @RequestMapping("/dev")
 public class DevController {
 
     private final ObservationRegistry observationRegistry;
+    private final SleepService sleepService;
+
+    @GetMapping("/sleep")
+    public Long sleep(
+            @RequestParam Long ms
+    ) {
+        //        return this.doSleepTimer.record(() -> this.doSleep(ms));
+        return sleepService.doSleep(ms);
+    }
 
     @GetMapping
     public String getDev() {
@@ -22,3 +38,18 @@ public class DevController {
         ).observe(() -> "Dev");
     }
 }
+
+@Service
+class SleepService {
+
+    @Timed(value = "do.sleep.method.timed")
+    public Long doSleep(Long ms) {
+        try {
+            TimeUnit.MILLISECONDS.sleep(ms);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+        return ms;
+    }
+}
+
